bfd2e5a51fbcdff4c9be8c26294b28ccffabf114
nil <lue.gsk.2012@gmail.com>
2018-12-21 16:33:38 +0900

    Fix Sample.scala

--- sample/src/main/scala/me/nsmr/lilib/sample/Sample.scala
+++ sample/src/main/scala/me/nsmr/lilib/sample/Sample.scala
@@ -4,6 +4,7 @@ package sample
 
 import akka.actor._
 import me.nsmr.lilib
+import lilib.core.Precedent
 import lilib.supremecourtdb.data.{ SupremeCourtDbDataProvider, FileSupremeCourtDbDataProvider }
 import lilib.search.{ PrecedentIndex, PrecedentIndexBuilder, ConcurrentPrecedentIndexBuilder }
 import scala.concurrent.ExecutionContext.Implicits.global
@@ -17,10 +18,22 @@ class Sample {
   implicit lazy val sys = ActorSystem("LIlib-Sample")
   lazy val logger = Logger[Sample]
 
-  val settings = sys.settings.config.root
-  logger.debug(settings.toString)
+  def createProvider: SupremeCourtDbDataProvider[String] = new FileSupremeCourtDbDataProvider
 
-  def getProvider: SupremeCourtDbDataProvider[String] = new FileSupremeCourtDbDataProvider
+  def createIndexBuilder: ConcurrentPrecedentIndexBuilder[String] = PrecedentIndexBuilder.concurrentBuilder[String]
 
-  def getIndexBuilder: ConcurrentPrecedentIndexBuilder[String] = PrecedentIndexBuilder.concurrentBuilder[String]
+  def getPrecedentList: Iterator[(String, Precedent)] = createProvider.iterator.map { data => (data.id, data.asPrecedent) }
+
+  def getPrecedentIndex(size: Int): PrecedentIndex[String] = {
+    using(createIndexBuilder) { builder =>
+      builder.add(getPrecedentList.take(size))
+      builder.build
+    }
+  }
+
+  def getPrecedentIndex: PrecedentIndex[String] = {
+    PrecedentIndexBuilder.concurrentlyBuilding { builder: ConcurrentPrecedentIndexBuilder[String] =>
+      builder.add(getPrecedentList)
+    }
+  }
 }
