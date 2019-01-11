0b85a5895cdd2fbb62ec8aa73b3d457076e5be39
nil <lue.gsk.2012@gmail.com>
2019-01-11 18:23:39 +0900

    Simplify SupremeCourtDbConnector
    不必要なPrecedentKeyオブジェクトを削除してSimplifyした

--- lilib-supremecourtdb/src/main/scala/me/nsmr/lilib/supremecourtdb/SupremeCourtDbConnector.scala
+++ lilib-supremecourtdb/src/main/scala/me/nsmr/lilib/supremecourtdb/SupremeCourtDbConnector.scala
@@ -10,84 +10,83 @@ import scala.util.Try
 
 object SupremeCourtDbLikeConnector {
   val SUPREME_COURT_DB_DETAIL_BASE_URL = "http://www.courts.go.jp/app/hanrei_jp/detail2"
-  class PrecedentKey(val name: String) { override def toString(): String = s"Key(${name})" }
-  object PrecedentKey {
-    case object CaseNumber extends PrecedentKey("事件番号")
-    case object CaseName extends PrecedentKey("事件名")
-    case object CaseDate extends PrecedentKey("裁判年月日")
-    case object Court extends PrecedentKey("法廷名")
-    case object JudgeType extends PrecedentKey("裁判種別")
-    case object Result extends PrecedentKey("結果")
-    case object Book extends PrecedentKey("判例集等巻・号・頁")
-    case object PreviousCourt extends PrecedentKey("原審裁判所名")
-    case object PreviousCaseNumber extends PrecedentKey("原審事件番号")
-    case object PreviousCaseDate extends PrecedentKey("原審裁判年月日")
-    case object Theme extends PrecedentKey("判示事項")
-    case object Summary extends PrecedentKey("裁判要旨")
-    case object RelatedArticles extends PrecedentKey("参照法条")
-    case object Content extends PrecedentKey("全文")
-
-    def keys = Array(
-      CaseNumber,
-      CaseName,
-      CaseDate,
-      Court,
-      JudgeType,
-      Result,
-      Book,
-      PreviousCourt,
-      PreviousCaseNumber,
-      PreviousCaseDate,
-      Theme,
-      Summary,
-      RelatedArticles,
-      Content
-    )
-
-    def apply(key: String): PrecedentKey = keys.find(k => k.name == key).getOrElse(new PrecedentKey(key))
-    def unapply(obj: PrecedentKey): Option[String] = Option(obj.name)
-  }
 }
 
 class SupremeCourtDbLikeConnector(val base: String) {
   import SupremeCourtDbLikeConnector._
+  type Parser = (String, Element) --> (String, Any)
 
   def getUrlOf(id: Int): String = s"${base}?id=${id}"
 
-  def specialRowParser: (PrecedentKey, Element) --> (PrecedentKey, Any) = {
-    case (PrecedentKey.Content, elem) => (PrecedentKey.Content, elem.selectFirst("a").attr("abs:href"))
-  }
+  def specialRowParser: Parser = Map.empty
 
-  def rowParser: (PrecedentKey, Element) --> (PrecedentKey, Any) = specialRowParser.orElse( {
+  def rowParser: Parser = specialRowParser.orElse( {
     case (key, elems) => (key, elems.children.tail.map(_.text).mkString.powerTrim)
-  }: (PrecedentKey, Element) --> (PrecedentKey, Any))
+  }: Parser)
 
-  def getPrecedentMapById(id: Int): Try[Map[PrecedentKey, Any]] = Try {
+  def getPrecedentMapById(id: Int): Try[Map[String, Any]] = Try {
     val doc = Jsoup.connect(getUrlOf(id)).get
     val element = doc.select("div.dlist").iterator.flatMap(_.children).filter(x => x.tagName == "div" && x.className.isEmpty)
     element.collect { case e if !(e.children.size < 2) =>
-      val key = PrecedentKey(e.children.head.text.powerTrim)
+      val key = e.children.head.text.powerTrim
       rowParser((key, e))
     }.toMap
   }
 }
 
-object SupremeCourtDbConnector extends SupremeCourtDbLikeConnector(SupremeCourtDbLikeConnector.SUPREME_COURT_DB_DETAIL_BASE_URL) {
+object SupremeCourtDbConnector extends SupremeCourtDbLikeConnector(
+  SupremeCourtDbLikeConnector.SUPREME_COURT_DB_DETAIL_BASE_URL
+) {
   import SupremeCourtDbLikeConnector._
 
-  override def specialRowParser: (PrecedentKey, Element) --> (PrecedentKey, String) = {
-    case (PrecedentKey.Content, elem) => (PrecedentKey.Content, elem.selectFirst("a").attr("abs:href"))
+  object Keys {
+    val number = "事件番号"
+    val name = "事件名"
+    val date = "裁判年月日"
+    val court = "法廷名"
+    val judgeType = "裁判種別"
+    val result = "結果"
+    val report = "判例集等巻・号・頁"
+    val originalCourt = "原審裁判所名"
+    val originalNumber = "原審事件番号"
+    val originalDate = "原審裁判年月日"
+    val theme = "判示事項"
+    val summary = "裁判要旨"
+    val relatedArticles = "参照法条"
+    val content = "全文"
+
+    def all = Set(
+      number,
+      name,
+      date,
+      court,
+      judgeType,
+      result,
+      report,
+      originalCourt,
+      originalNumber,
+      originalDate,
+      theme,
+      summary,
+      relatedArticles,
+      content,
+    )
   }
 
-  override def rowParser: (PrecedentKey, Element) --> (PrecedentKey, String) = specialRowParser.orElse( {
-    case (key, elems) => (key, elems.children.tail.map(_.text).mkString.powerTrim)
-  }: (PrecedentKey, Element) --> (PrecedentKey, String))
+  override def specialRowParser: (String, Element) --> (String, String) = {
+    case (Keys.content, elem) => (Keys.content, elem.selectFirst("a").attr("abs:href"))
+  }
 
-  override def getPrecedentMapById(id: Int): Try[Map[PrecedentKey, String]] = Try {
+  override def rowParser: (String, Element) --> (String, String) = specialRowParser.orElse( {
+    // 本当はここに、意図されていないキーが含まれていた場合の処理も書いておきたい
+    case (key, elems) => (key, elems.children.tail.map(_.text).mkString.powerTrim)
+  }: (String, Element) --> (String, String))
+
+  override def getPrecedentMapById(id: Int): Try[Map[String, String]] = Try {
     val doc = Jsoup.connect(getUrlOf(id)).get
     val element = doc.select("div.dlist").iterator.flatMap(_.children).filter(x => x.tagName == "div" && x.className.isEmpty)
     element.collect { case e if !(e.children.size < 2) =>
-      val key = PrecedentKey(e.children.head.text.powerTrim)
+      val key = e.children.head.text.powerTrim
       rowParser((key, e))
     }.toMap
   }
