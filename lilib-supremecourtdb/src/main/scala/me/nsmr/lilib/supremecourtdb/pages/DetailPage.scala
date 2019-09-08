e4cd0fd8aac7881490eef21da8bc73a92df86ebb
nil <lue.gsk.2012@gmail.com>
2019-09-08 15:17:11 +0900

    Add Page classes

---
+++ lilib-supremecourtdb/src/main/scala/me/nsmr/lilib/supremecourtdb/pages/DetailPage.scala
@@ -0,0 +1,105 @@
+package me.nsmr
+package lilib
+package supremecourtdb
+package pages
+
+import org.jsoup.Jsoup
+import org.jsoup.HttpStatusException
+import org.jsoup.nodes.Element
+import scala.collection.JavaConverters._
+
+object DetailPage {
+  val SUPREME_COURT_DB_DETAIL_BASE_URL = "http://www.courts.go.jp/app/hanrei_jp/detail"
+
+  sealed class PrecedentKey(val name: String) {
+    override def toString(): String = s"Key(${name})"
+  }
+
+  object PrecedentKey {
+
+    object CaseNumber extends PrecedentKey("事件番号")
+
+    object CaseName extends PrecedentKey("事件名")
+
+    object CaseDate extends PrecedentKey("裁判年月日")
+
+    object Court extends PrecedentKey("法廷名")
+
+    object JudgeType extends PrecedentKey("裁判種別")
+
+    object Result extends PrecedentKey("結果")
+
+    object Book extends PrecedentKey("判例集等巻・号・頁")
+
+    object PreviousCourt extends PrecedentKey("原審裁判所名")
+
+    object PreviousCaseNumber extends PrecedentKey("原審事件番号")
+
+    object PreviousCaseDate extends PrecedentKey("原審裁判年月日")
+
+    object Theme extends PrecedentKey("判示事項")
+
+    object Summary extends PrecedentKey("裁判要旨")
+
+    object RelatedArticles extends PrecedentKey("参照法条")
+
+    object Content extends PrecedentKey("全文")
+
+    def keys = Array(
+      CaseNumber,
+      CaseName,
+      CaseDate,
+      Court,
+      JudgeType,
+      Result,
+      Book,
+      PreviousCourt,
+      PreviousCaseNumber,
+      PreviousCaseDate,
+      Theme,
+      Summary,
+      RelatedArticles,
+      Content
+    )
+
+    def apply(key: String): PrecedentKey = keys.find(k => k.name == key).getOrElse(new PrecedentKey(key))
+
+    def unapply(obj: PrecedentKey): Option[String] = Option(obj.name)
+  }
+
+  def getUrlOf(pageType: Int, id: Int): String = s"${SUPREME_COURT_DB_DETAIL_BASE_URL}${pageType}?id=${id}"
+}
+
+class DetailPage(val id: Int, val pageType: Int = 2) {
+
+  import DetailPage.PrecedentKey
+
+  def url = DetailPage.getUrlOf(pageType, id)
+
+  lazy val doc = try {
+    Some(Jsoup.connect(url).get)
+  } catch {
+    case e: HttpStatusException => None
+  }
+
+  lazy val content: Option[Map[String, Element]] = doc.flatMap { doc =>
+    val elements =
+      doc.select("div.dlist").iterator.asScala.flatMap(_.children.asScala).filter { x =>
+        x.tagName == "div" && x.className.isEmpty
+      }
+    Option(
+      elements.collect {
+        // もうちょっと包括的に定義できないか
+        case e if !(e.children.size < 2) => {
+          val key = e.children.asScala.head.text.trim
+          if(PrecedentKey.Content.name == key) {
+            (key, e.selectFirst("a"))
+          } else {
+            (key, e.child(1))
+          }
+        }
+      }.toMap
+    )
+  }
+}
+
---
+++ lilib-supremecourtdb/src/main/scala/me/nsmr/lilib/supremecourtdb/pages/ListPage.scala
@@ -0,0 +1,178 @@
+package me.nsmr
+package lilib
+package supremecourtdb
+package pages
+
+//import java.time.chrono.{JapaneseChronology, JapaneseDate}
+
+import java.net.URLEncoder
+import java.time.temporal.ChronoField
+import java.util.Date
+import org.jsoup.nodes.Element
+import scala.collection.JavaConverters._
+
+import me.nsmr.lilib.core.{CaseMark, CaseNumber, CaseYear, Court}
+import org.jsoup.Jsoup
+
+object ListPage {
+
+  case class JapaneseDate(year: CaseYear, month: Int, date: Int)
+
+  object SearchFilter {
+    def all = SearchFilter(
+      None,
+      None,
+      SpanSpecifier(
+        JapaneseDate(CaseYear(CaseYear.Era.Showa, 1), 1, 1),
+        JapaneseDate(CaseYear(CaseYear.Era.Reiwa, 99), 12, 31)
+      ),
+      Nil
+    )
+  }
+
+  case class SearchFilter(
+                           court: Option[Court],
+                           caseNumber: Option[CaseNumber],
+                           dateSpecifier: DateSpecifier,
+                           texts: List[String],
+                         ) {
+    private def courtToMap(court: Option[Court]): Map[String, String] = Map(
+      "courtName" -> court.map(_.place).getOrElse(""),
+      "courtType" -> court.map(_.level.toString).getOrElse(""),
+      "branchName" -> court.map(_.branch).getOrElse("")
+    )
+
+    private def caseNumberToMap(caseNumber: Option[CaseNumber]): Map[String, String] = Map(
+      "jikenGengo" -> caseNumber.map(_.year.era.name).getOrElse(""),
+      "jikenYear" -> caseNumber.map(_.year.year.toString).getOrElse(""),
+      "jikenCode" -> caseNumber.map(_.mark.toString).getOrElse(""),
+      "jikenNumber" -> caseNumber.map(_.index.toString).getOrElse("")
+    )
+
+    def asQuery: String = {
+      import URLEncoder.encode
+      (courtToMap(court)
+        ++ caseNumberToMap(caseNumber)
+        ++ dateSpecifier.asMap
+        ++ texts.zipWithIndex.map { case (text, idx) =>
+        s"text${idx + 1}" -> text
+      }.toMap + ("action_search" -> "検索")
+        ).map {
+        case (key, value) =>
+          s"filter[${encode(key, "UTF-8")}]=${encode(value, "UTF-8")}"
+      }.mkString("&")
+    }
+  }
+
+  sealed trait DateSpecifier {
+    def asMap: Map[String, String]
+  }
+
+  case object NoSpecify extends DateSpecifier {
+    override def asMap: Map[String, String] = Map(
+      "judgeDateMode" -> "",
+      "judgeGengoFrom" -> "",
+      "judgeYearFrom" -> "",
+      "judgeMonthFrom" -> "",
+      "judgeDayFrom" -> "",
+      "judgeGengoFrom" -> "",
+      "judgeYearTo" -> "",
+      "judgeMonthTo" -> "",
+      "judgeDayTo" -> "",
+    )
+  }
+
+  case class SingleDateSpecifier(date: JapaneseDate) extends DateSpecifier {
+    override def asMap: Map[String, String] = Map(
+      "judgeDateMode" -> "1",
+      "judgeGengoFrom" -> date.year.era.name,
+      "judgeYearFrom" -> date.year.year.toString,
+      "judgeMonthFrom" -> date.month.toString,
+      "judgeDayFrom" -> date.date.toString,
+      "judgeGengoTo" -> "",
+      "judgeYearTo" -> "",
+      "judgeMonthTo" -> "",
+      "judgeDayTo" -> ""
+    )
+  }
+
+  case class SpanSpecifier(from: JapaneseDate, to: JapaneseDate) extends DateSpecifier {
+    override def asMap: Map[String, String] = Map(
+      "judgeDateMode" -> "2",
+      "judgeGengoFrom" -> from.year.era.name,
+      "judgeYearFrom" -> from.year.year.toString,
+      "judgeMonthFrom" -> from.month.toString,
+      "judgeDayFrom" -> from.date.toString,
+      "judgeGengoTo" -> to.year.era.name,
+      "judgeYearTo" -> to.year.year.toString,
+      "judgeMonthTo" -> to.month.toString,
+      "judgeDayTo" -> to.date.toString,
+    )
+  }
+
+  val BASE_URL = "http://www.courts.go.jp/app/hanrei_jp/list"
+
+  def getUrlOf(pageType: Int, params: SearchFilter): String = {
+    s"${BASE_URL}${pageType}?${params.asQuery}"
+  }
+
+  def getUrlOf(pageType: Int, params: SearchFilter, page: Int): String = {
+    s"${BASE_URL}${pageType}?${params.asQuery}&page=${page}"
+  }
+
+  private lazy val DETAIL_URL_PATTERN = s"${DetailPage.SUPREME_COURT_DB_DETAIL_BASE_URL}[0-9]+\\?id=([0-9]+)".r
+}
+
+class ListPage(
+                val pageType: Int = 1,
+                val params: ListPage.SearchFilter,
+                val page: Option[Int] = None
+              ) {
+  def url: String = page match {
+    case None => ListPage.getUrlOf(pageType, params)
+    case Some(page) => ListPage.getUrlOf(pageType, params, page)
+  }
+
+  lazy val doc = Jsoup.connect(url).get
+
+  lazy val content: (List[(Element, Element, Element)], Option[Element]) = {
+    (
+      doc.select("div#list tr").asScala.map {
+        elem => {
+          (
+            elem.selectFirst("td > a"),
+            elem.select("td").get(1),
+            elem.select("td > a").get(1))
+        }
+      }.toList,
+      Option(doc.selectFirst("div#next_buttom > div.s_title_r > a"))
+    )
+  }
+
+  def nextUrl: Option[String] = content._2.map(_.attr("abs:href"))
+
+  def hasNext: Boolean = nextUrl.isDefined
+
+  /** ちょっと妥協した。本当はnextUrlをパースしてListPageを再構築したい。 */
+  def next: Option[ListPage] = if (!hasNext) None else {
+    Option(new ListPage(
+      pageType, params,
+      page match {
+        case None => Some(1)
+        case Some(page) => Some(page + 1)
+      }
+    ))
+  }
+
+  def details: List[DetailPage] = content._1.map {
+    case (elem, _, _) =>
+      elem.attr("abs:href") match {
+        case ListPage.DETAIL_URL_PATTERN(id) => new DetailPage(id.toInt)
+        case url => throw new IllegalArgumentException(s"Illegal Pattern of URL: ${url}")
+      }
+  }
+
+  def size: Int = content._1.size
+
+}
+
